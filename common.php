955bc9271c2a84845c26ffcf9c314164786def87
dev <toplss@nsmg21.com>
2024-07-05 17:26:11 +0900

    feat : 플레이트 설치
    common 파일 require 'vendor/autoload.php';
    적용

--- common.php
+++ common.php
@@ -844,6 +844,10 @@ if($is_member && !$is_admin && (!defined("G5_CERT_IN_PROG") || !G5_CERT_IN_PROG)
 
 ob_start();
 
+// 플레이트 설치
+require 'vendor/autoload.php';
+
+
 // 자바스크립트에서 go(-1) 함수를 쓰면 폼값이 사라질때 해당 폼의 상단에 사용하면
 // 캐쉬의 내용을 가져옴. 완전한지는 검증되지 않음
 header('Content-Type: text/html; charset=utf-8');
